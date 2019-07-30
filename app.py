a3e97d9fa3501b188a78010c2895b66692eb3a44
myname21-meet <myname21@meet.mit.edu>
2019-07-30 03:29:53 -0400

    h

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home page')
 def home():
-    return "home page" 
+	return "WELCOME HOME" 
+@app.route('/student/<int:student_id>/<string:name>/<int:student_year')
+def student_id(student_id):
+	return render_template(
+		"student.html", student_id = student_id,)
 
 if __name__ == '__main__':
     app.run(debug=True)
